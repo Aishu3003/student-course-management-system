858a68d6fffebb09eb551ef76ce10a6bb5b2afd6
Aishu3003 <aishwaryachaudhari3003@gmail.com>
2024-03-07 11:46:04 -0800

    commit

---
+++ student-course-management-system-backend/src/main/java/com/app/Entities/Course.java
@@ -0,0 +1,19 @@
+package com.app.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+   
+    
+    
+}
